c46fe7556f65faef886feb3aa815578129f7eb64
Anushka Sinha <evilanushka968@gmail.com>
2025-09-20 02:23:17 +0530

    creating my potfolio and deploying

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed((".skills"),{
+    strings : ["Engineer In The Making","Web Development","Undergraduate"],
+    typeSpeed : 100,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+})
